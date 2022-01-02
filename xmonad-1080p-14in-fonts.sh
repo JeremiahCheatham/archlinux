e7c4205d7a27a5b99e41eb45e5068a9856e26ffb
JeremiahCheatham <72688505+JeremiahCheatham@users.noreply.github.com>
2022-01-02 23:35:01 +0000

    Update xmonad-1080p-14in-fonts.sh
    Added font size for dmenu. Also changed some fonts to Noto Sans.

--- xmonad-1080p-14in-fonts.sh
+++ xmonad-1080p-14in-fonts.sh
@@ -1,20 +1,23 @@
 #!/bin/fish
 
+sed -i 's/dmenu -i -l 20 -p/dmenu -fn xft:size=14 -i -l 20 -p/' ~/.config/dmscripts/config
+
+sed -i 's/dmenu_run -i -p/dmenu_run -fn xft:size=14 -i -p/' ~/.xmonad/xmonad.hs
+sed -i 's/SauceCodePro Nerd Font Mono:regular:size=9/Noto Sans:regular:size=14/' ~/.xmonad/xmonad.hs
+
 sed -i 's/family: Source Code Pro/# family: Source Code Pro/' ~/.config/alacritty/alacritty.yml
 sed -i 's/# family: Hack/family: Hack/' ~/.config/alacritty/alacritty.yml
 sed -i 's/size: 12.0/size: 14.0/' ~/.config/alacritty/alacritty.yml
 
-sed -i 's/SauceCodePro Nerd Font Mono:regular:size=9/SauceCodePro Nerd Font Mono:regular:size=14/' ~/.xmonad/xmonad.hs
-
-sed -i 's/Ubuntu:weight=bold:pixelsize=11/Ubuntu:weight=bold:pixelsize=14/' ~/.config/xmobar/*-xmobarrc
+sed -i 's/Ubuntu:weight=bold:pixelsize=11/Noto Sans:weight=bold:pixelsize=14/' ~/.config/xmobar/*-xmobarrc
 sed -i 's/Mononoki:pixelsize=11/Mononoki:pixelsize=14/' ~/.config/xmobar/*-xmobarrc
 sed -i 's/Font Awesome 5 Free Solid:pixelsize=12/Font Awesome 5 Free Solid:pixelsize=15/' ~/.config/xmobar/*-xmobarrc
 sed -i 's/Font Awesome 5 Brands:pixelsize=12/Font Awesome 5 Brands:pixelsize=15/' ~/.config/xmobar/*-xmobarrc
 
-sed -i 's/Source Code Pro:bold:size=10/Source Code Pro:bold:size=12/' ~/.config/conky/xmonad/*.conkyrc
-sed -i 's/Raleway:bold:size=30/Raleway:bold:size=44/' ~/.config/conky/xmonad/*.conkyrc
-sed -i 's/Ubuntu:size=14/Ubuntu:size=18/' ~/.config/conky/xmonad/*.conkyrc
-sed -i 's/Raleway:bold:size=9/Raleway:bold:size=14/' ~/.config/conky/xmonad/*.conkyrc
+sed -i 's/Source Code Pro:bold:size=10/Noto Sans:bold:size=11/' ~/.config/conky/xmonad/*.conkyrc
+sed -i 's/Raleway:bold:size=30/Noto Sans:bold:size=38/' ~/.config/conky/xmonad/*.conkyrc
+sed -i 's/Ubuntu:size=14/Noto Sans:size=16/' ~/.config/conky/xmonad/*.conkyrc
+sed -i 's/Raleway:bold:size=9/Noto Sans:bold:size=12/' ~/.config/conky/xmonad/*.conkyrc
 
 sed -i 's/gtk-font-name=Noto Sans,  10/gtk-font-name=Noto Sans,  14/' ~/.config/gtk-*/settings.ini
 
